f484699f011ad8d18ad57b87aa7eea2e39dd5eef
E-gap <106596181+E-gap@users.noreply.github.com>
2023-08-22 13:51:09 +0300

    update styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,12 @@
 import css from './HomePage.module.css';
 import { NavLink } from 'react-router-dom';
-import { AnimationHome } from '../../components/AnimationHome/AnimationHome';
+
 import Container from 'components/Container/Container';
 
 const HomePage = () => {
   return (
     <div className={css.homePage}>
       <Container>
-        <AnimationHome />
         <NavLink to="/cars" className={css.moveToCards}>
           Go to Cars
         </NavLink>
