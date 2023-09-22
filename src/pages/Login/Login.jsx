0184ac6a0eb3102016a3faf1d4feecf6ec4c2c7b
Evhen Gaponyuk <e-gap@ukr.net>
2023-09-22 20:36:53 +0300

    update components and pages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,100 +1,15 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import Container from 'components/Container/Container';
-import * as Yup from 'yup';
-import { useState } from 'react';
 import css from './Login.module.css';
 // import PropTypes from 'prop-types';
-import { BiShow } from 'react-icons/bi';
-import { /* register, */ login } from '../../redux/auth/authOperations';
-import { useDispatch /* useSelector */ } from 'react-redux';
-import Button from '../../components/Button/Button';
-// import { selectIsLogin } from '../../redux/selectors';
-import { useNavigate } from 'react-router-dom';
+import FormSign from 'components/FormSign/FormSign';
 
-function Login({ sign, closeModal }) {
-  const navigate = useNavigate();
-  // const isLogin = useSelector(selectIsLogin);
-  const [typePassword, setTypePassword] = useState('password');
-  // const [typeConfirmPassword, setTypeConfirmPassword] = useState('password');
-  const dispatch = useDispatch();
-  /* const [signValue, setSignValue] = useState(() => {
-    return sign;
-  }); */
-
-  const SignInSchema = Yup.object().shape({
-    email: Yup.string().required('Please input email'),
-    password: Yup.string().required('Please input password'),
-  });
-
-  const submitForm = (values, actions) => {
-    actions.resetForm();
-    const userDataForLogin = {
-      email: values.email,
-      password: values.password,
-    };
-
-    dispatch(login(userDataForLogin));
-  };
-
-  const toggleShowPassword = () => {
-    if (typePassword === 'password') {
-      setTypePassword('text');
-    } else {
-      setTypePassword('password');
-    }
-  };
-
-  const handleButton = () => {
-    navigate('/register');
-  };
-
+function Login() {
   return (
     <div className={css.loginPage}>
       <Container>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          validationSchema={SignInSchema}
-          onSubmit={submitForm}
-        >
-          <Form className={css.form}>
-            <label className={css.label}>
-              E-mail
-              <Field name="email" />
-              <ErrorMessage
-                name="email"
-                render={message => (
-                  <div className={css.errorValidation}>{message}</div>
-                )}
-              />
-            </label>
-            <label className={css.label}>
-              Password
-              <Field type={typePassword} name="password" />
-              <BiShow className={css.buttonHide} onClick={toggleShowPassword} />
-              <ErrorMessage
-                name="password"
-                render={message => (
-                  <div className={css.errorValidation}>{message}</div>
-                )}
-              />
-            </label>
-            <button type="submit" className={css.submit}>
-              Submit
-            </button>
-          </Form>
-        </Formik>
-        <p className={css.question}>
-          Are you not Signed Up?
-          <Button
-            text="Sign Up"
-            handleButton={handleButton}
-            view="buttonQuestionSign"
-          />
-        </p>
+        <h1 className={css.titlePage}>Login</h1>
+        <FormSign sign="signIn" />
       </Container>
     </div>
   );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,146 +1,15 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { useState } from 'react';
 import css from './Register.module.css';
 import Container from 'components/Container/Container';
+import FormSign from 'components/FormSign/FormSign';
 // import PropTypes from 'prop-types';
-import { BiShow } from 'react-icons/bi';
-import { register /* login */ } from '../../redux/auth/authOperations';
-import { useDispatch /* useSelector */ } from 'react-redux';
-import Button from '../../components/Button/Button';
-// import { selectIsLogin } from '../../redux/selectors';
-import { useNavigate } from 'react-router-dom';
 
-function Register({ sign, closeModal }) {
-  const navigate = useNavigate();
-  // const isLogin = useSelector(selectIsLogin);
-  const [typePassword, setTypePassword] = useState('password');
-  const [typeConfirmPassword, setTypeConfirmPassword] = useState('password');
-  const dispatch = useDispatch();
-  /* const [signValue, setSignValue] = useState(() => {
-    return sign;
-  }); */
-
-  const SignUpSchema = Yup.object().shape({
-    name: Yup.string().required('Please input name'),
-    email: Yup.string().required('Please input email'),
-    password: Yup.string().required('Please input password'),
-    confirmPassword: Yup.string().oneOf(
-      [Yup.ref('password'), null],
-      'Passwords must match'
-    ),
-  });
-
-  const submitForm = (values, actions) => {
-    actions.resetForm();
-    const userDataForRegister = {
-      name: values.name,
-      email: values.email,
-      password: values.password,
-    };
-
-    dispatch(register(userDataForRegister));
-  };
-
-  const toggleShowPassword = () => {
-    if (typePassword === 'password') {
-      setTypePassword('text');
-    } else {
-      setTypePassword('password');
-    }
-  };
-
-  const toggleShowConfirmPassword = () => {
-    if (typeConfirmPassword === 'password') {
-      setTypeConfirmPassword('text');
-    } else {
-      setTypeConfirmPassword('password');
-    }
-  };
-
-  const handleButton = e => {
-    navigate('/login');
-  };
-
+function Register() {
   return (
     <div className={css.registerPage}>
       <Container>
-        <Formik
-          initialValues={{
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-          }}
-          validationSchema={SignUpSchema}
-          onSubmit={submitForm}
-        >
-          <Form className={css.form}>
-            <label className={css.label}>
-              Name
-              <Field name="name" />
-              <ErrorMessage
-                name="name"
-                render={message => (
-                  <div className={css.errorValidation}>{message}</div>
-                )}
-              />
-            </label>
-
-            <label className={css.label}>
-              E-mail
-              <Field name="email" />
-              <ErrorMessage
-                name="email"
-                render={message => (
-                  <div className={css.errorValidation}>{message}</div>
-                )}
-              />
-            </label>
-            <label className={css.label}>
-              Password
-              <Field type={typePassword} name="password" />
-              <BiShow className={css.buttonHide} onClick={toggleShowPassword} />
-              <ErrorMessage
-                name="password"
-                render={message => (
-                  <div className={css.errorValidation}>{message}</div>
-                )}
-              />
-            </label>
-
-            <label className={css.label}>
-              Confirm password
-              <Field type={typeConfirmPassword} name="confirmPassword" />
-              <BiShow
-                className={css.buttonHide}
-                onClick={toggleShowConfirmPassword}
-              />
-              <ErrorMessage
-                name="confirmPassword"
-                render={message => (
-                  <div className={css.errorValidation}>
-                    Passwords must match
-                  </div>
-                )}
-              />
-            </label>
-
-            <button type="submit" className={css.submit}>
-              Submit
-            </button>
-          </Form>
-        </Formik>
-
-        <p className={css.question}>
-          Are you Signed Up?
-          <Button
-            text="Sign In"
-            handleButton={handleButton}
-            view="buttonQuestionSign"
-          />
-        </p>
+        <h1 className={css.titlePage}>Register</h1>
+        <FormSign sign="signUp" />
       </Container>
     </div>
   );
