ed84bf6bc37325b6dff387bee1da08e7283a45e6
Evhen Gaponyuk <e-gap@ukr.net>
2023-09-27 14:14:19 +0300

    suspense

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,12 +2,13 @@ import Header from '../Header/Header';
 import Footer from 'components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import Preloader from 'components/Preloader/Preloader';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
-      <Suspense>
+      <Suspense fallback={<Preloader />}>
         <Outlet />
       </Suspense>
       <Footer />
